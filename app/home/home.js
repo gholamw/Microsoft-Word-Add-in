5479f5994b111bc8f5f3705d9f922a28ccd08be8
DaveB95 <boothmad@tcd.ie>
2016-03-12 19:39:34 +0000

    Get Data From Selection
    Added get data from selection content

--- app/home/home.js
+++ app/home/home.js
@@ -26,3 +26,36 @@ function UpdateTimer() {
 function LeadingZero(Time) {
     return (Time < 10) ? "0" + Time : + Time;
 }
+
+(function(){
+  'use strict';
+
+  // The initialize function must be run each time a new page is loaded
+  Office.initialize = function(reason){
+    jQuery(document).ready(function(){
+      app.initialize();
+
+      jQuery('#get-data-from-selection').click(getDataFromSelection);
+    });
+  };
+
+  // Reads data from current document selection and displays a notification
+  function getDataFromSelection(){
+    Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+      function(result){
+        if (result.status === Office.AsyncResultStatus.Succeeded) {
+          app.showNotification('The selected text is:', '"' + result.value + '"');
+          var newResult = result.value.split(' ').join('+');
+          print(newResult);
+          var xmlHttp = new XMLHttpRequest();
+          xmlHttp.open( "GET", "http://inspiremetcd.azurewebsites.net/api?q=" + newResult, false ); // false for synchronous request
+          xmlHttp.send( null );
+    return xmlHttp.responseText;
+        } else {
+          app.showNotification('Error:', result.error.message);
+        }
+      }
+    );
+  }
+
+})();
